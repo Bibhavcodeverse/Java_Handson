f08c3d4b2eafa850bf1bdb8f500611a77a5de12b
Bibhav kumar <bibhavkumar05@gmail.com>
2024-09-02 00:00:51 +0530

    Create java_beginner.java

---
+++ java_beginner.java
@@ -0,0 +1,102 @@
+
+import java.util.Arrays;
+public class Main
+{
+	public static void main(String[] args) {
+// 	    String name ="Aman";
+// 	    String name2 = name.replace('a','b');
+// 	    System.out.println(name2);
+	    
+// 		System.out.println(name);
+
+        // String name ="Aman and Akku";
+        // System.out.println(name.substring(5,9));
+        
+        // int age= 30;
+        // int physics =97;
+        // int chem= 87;
+        // int eng = 87;
+        
+        //Array 
+        
+        // int[] marks = new int[3];
+        // marks[0]= 97;
+        // marks[1]=87;
+        // marks[2]=98;
+        // System.out.println(marks[0]);
+        // //length of Array
+        // //System.out.println(marks.length);
+        // Arrays.sort(marks);
+        
+        // System.out.println(marks[0]);
+        // int[] marks ={97,98,95,97,90,89,60};
+        // System.out.println();
+        //int[][] finalMarks = {{97,98,95},{89,87,90}};
+        //System.out.println(finalMarks[0][0]);
+        //System.out.println(finalMarks[1][1]);
+        //casting
+        
+        //  int p=100;
+        // //  int fp=p+(int)18.9999;
+        // //  System.out.println(fp);
+         
+        //  double price = 100.0;
+        //  double finalPrice = price + 18.999;
+        //  System.out.println(finalPrice);
+         
+         // Constants 
+        //  int age = 30;
+        //  age =31;
+        //  age =32;
+        //  final float PI = 3.14F;
+         
+        //  //PI = 1.1F;  not changeable 
+        //  System.out.println(PI);
+         
+         
+        //  operators
+         
+        //  int a=5;
+        //  int b=6;
+        //  int  sum=a+b;
+        //int div=a/b;
+        //  System.out.println(sum);
+        // System.out.println(diff);
+        //  System.out.println(mul);
+        // System.out.println(div);
+        //  double a=5;
+        //  double b=6;
+        //  double div=a/b;
+         
+        //  System.out.println(div);
+        
+        
+        //  double a=5;
+        //  double b=6;
+        // double modulo=a%b;
+         
+        // System.out.println(modulo);
+       // increment and decrement operator  
+       int x =1;
+       int y=2;
+       
+    //   int num = (x++) +(--y);
+    //   int num1 = (++x)+(x--)
+    //   System.out.println(num);
+    //   System.out.println()
+    
+    //   int num2= (++x)+(x--);
+    //   System.out.println(num2);
+    
+    // int num3 = (++x)+(y--)-(--x);
+    // System.out.println(num3);
+        
+        
+        
+         
+         
+         
+        
+        
+	}
+}
