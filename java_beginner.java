9fc912ac6d154661bc241fb0906d94366b034690
Bibhav kumar <bibhavkumar05@gmail.com>
2024-09-03 03:16:37 +0530

    java_beginner.java

--- java_beginner.java
+++ java_beginner.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 import java.util.Arrays;
 public class Main
 {
@@ -143,7 +143,23 @@ public class Main
   // if(a<50 || b<50)
   //       System.out.println("atleast one less than 50");
     
+    // Scanner sc = new Scanner(System.in);
+    // // pen =10; notebook =40
     
+    // int cash = sc.nextInt();
+    // if(cash<10) {
+    //     System.out.println("Cannot buy anything");
+    //     System.out.println("get more cash");
+        
+    // }
+    
+    // else if(cash>10 && cash<50)  {
+    //     System.out.println("can get 1 things");
+    // }
+    
+    // else {
+    //     System.out.println("can get both" );
+    // }
         
         
          
