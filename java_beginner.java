01c16bd101c943b581d8b229f84ac9e4efd0fef1
Bibhav kumar <bibhavkumar05@gmail.com>
2024-09-03 02:27:08 +0530

    java_beginner.java

--- java_beginner.java
+++ java_beginner.java
@@ -140,6 +140,9 @@ public class Main
     
     // if (a<50 && b<50)
     // System.out.println("both less than 50");
+  // if(a<50 || b<50)
+  //       System.out.println("atleast one less than 50");
+    
     
         
         
