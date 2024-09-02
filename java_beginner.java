c15d4b0556ea1c470dd3afc76c393cd91b28bf99
Bibhav kumar <bibhavkumar05@gmail.com>
2024-09-03 03:34:23 +0530

    java_beginner.java

--- java_beginner.java
+++ java_beginner.java
@@ -160,6 +160,20 @@ public class Main
     // else {
     //     System.out.println("can get both" );
     // }
+
+// switch case:
+//         int day =1;
+//         switch(day) {
+//             case 1 :
+//                 System.out.println("monday");
+//                 break;
+//             case 2:
+//                 System.out.println("tuesday");
+//                 break;
+//             case 3:
+//                 System.out.println("wed- sun");
+                
+//         }
         
         
          
